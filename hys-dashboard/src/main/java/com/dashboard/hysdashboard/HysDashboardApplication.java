02c87e63d4fdf7caf660d60926e0f021561a0671
Rakshit Chauhan <rakshit.chauhan@ephesoft.com>
2023-10-03 18:36:39 +0530

    Changes related to hystrix- dash

--- hys-dashboard/src/main/java/com/dashboard/hysdashboard/HysDashboardApplication.java
+++ hys-dashboard/src/main/java/com/dashboard/hysdashboard/HysDashboardApplication.java
@@ -1,16 +1,17 @@
-package com.project.gateway;
+package com.dashboard.hysdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
-public class ApiGatewayApplication {
+@EnableHystrixDashboard
+public class HysDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(HysDashboardApplication.class, args);
 	}
 
 }
